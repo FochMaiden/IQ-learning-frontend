ae1c0b1d0fa22f63f76d1447068147dae411f8e0
Gosia <gosia.xq@gmail.com>
2019-10-22 17:59:02 +0200

    logout changes

--- src/components/nav/Navbar.vue
+++ src/components/nav/Navbar.vue
@@ -1,15 +1,22 @@
 <template>
   <nav>
     <v-app-bar absolute app>
-      <v-app-bar-nav-icon class="grey--text" v-on:click="drawer = !drawer"> </v-app-bar-nav-icon>
+      <v-app-bar-nav-icon v-on:click="drawer = !drawer"> </v-app-bar-nav-icon>
       <v-toolbar-title>Title</v-toolbar-title>
-	    <v-spacer></v-spacer>
-	    <v-toolbar-items>
-		    <v-btn v-if="this.$auth.check()" v-on:click="logout" > {{this.$auth.user().username}} Logout </v-btn>
-		    <v-btn v-else text :to="link.link" v-for="link in links">{{link.title}}</v-btn>
-	    </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text color="primary" v-if="this.$auth.check()"
+          >{{ this.$auth.user().username }}
+        </v-btn>
+        <v-btn text color="primary"  v-if="this.$auth.check()" v-on:click="logout">
+          <v-icon small color="primary">fas fa-sign-out-alt</v-icon>
+        </v-btn>
+        <v-btn v-else text :to="link.link" v-for="link in links">{{
+          link.title
+        }}</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-    <v-navigation-drawer  v-model="drawer" app>
+    <v-navigation-drawer v-model="drawer" app>
       <p>aaa</p>
     </v-navigation-drawer>
   </nav>
@@ -19,21 +26,21 @@
 export default {
   name: "Navbar",
   data() {
-	return {
-	  drawer: false,
-	  links: [
-		{ title: "Login",  link: "/login" },
-		{ title: "Register",  link: "/register" },
-	  ]
-	}
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", link: "/login" },
+        { title: "Register", link: "/register" }
+      ]
+    };
   },
   methods: {
-	logout() {
-	  this.$auth.logout({
-	  redirect: { name: "home" }
-	  });
-	}
-  },
+    logout() {
+      this.$auth.logout({
+        redirect: { name: "home" }
+      });
+    }
+  }
 };
 </script>
 
