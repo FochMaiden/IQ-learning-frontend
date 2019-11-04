f40da97482d40f021e53d6d83b221cbae37e510e
karolkkk <caroline@bbod.io>
2019-11-04 06:52:51 +0100

    WIP:removing questions

--- src/pages/questions/ButtonCounter.vue
+++ src/pages/questions/ButtonCounter.vue
@@ -9,10 +9,10 @@
           block
           style="background-image: linear-gradient(to right, #fe7676, #f7717e, #ee6d85, #e46a8c, #d96891);"
           >Add Answer</v-btn
-        ></v-flex
-      >
+        >
+      </v-flex>
     </v-layout>
-    <v-layout row v-for="row in rows">
+    <v-layout row v-for="(row, index) in rows" :key="index" :row="row">
       <v-flex xs12 sm6 offset-sm3>
         <v-text-field
           :name="row.answer"
@@ -22,6 +22,20 @@
           filled
           v-model="row.answerValue"
         ></v-text-field>
+        <v-switch v-model="correct" label="correct"></v-switch>
+        <v-layout row>
+          <v-flex xs12 sm6 offset-sm3>
+            <v-btn
+              v-bind="row.answer"
+              v-on:click="removeRow"
+              large
+              dark
+              block
+              style="background-image: linear-gradient(to right, #fe7676, #f7717e, #ee6d85, #e46a8c, #d96891);"
+              >remove Answer</v-btn
+            >
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -31,8 +45,11 @@ export default {
   data() {
     return {
       currentInputIndex: 0,
+      id: 0,
+      correct: false,
       inputs: ['answer'],
       rows: [],
+      values: {},
     };
   },
   methods: {
@@ -42,6 +59,10 @@ export default {
         answerValue: '',
       });
       this.currentInputIndex++;
+    },
+    removeRow() {
+      //this.rows.splice(this.row[index],1);
+      console.log(this.rows[0]);
     },
   },
 };
