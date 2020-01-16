5a82c6b6adeb37b27b09f74e77f782244852c1a5
FochMaiden <gosia.xq@gmail.com>
2020-01-16 18:28:43 +0100

    WIP: chat

--- src/pages/user/Chat.vue
+++ src/pages/user/Chat.vue
@@ -2,19 +2,28 @@
   <v-container>
     <v-row>
       <v-col cols="4s">
-        <v-card>
+        <v-card elevation="4">
           <v-list dense subheader>
             <v-subheader>Recent chat</v-subheader>
-            <v-list-item dense v-for="user in users" :key="user.name" @click="selectUser(user)">
+            <v-list-item
+              dense
+              v-for="user in users"
+              :key="user.name"
+              @click="selectUser(user)"
+            >
               <v-list-item-avatar>
                 <v-img :src="user.avatar"></v-img>
               </v-list-item-avatar>
 
               <v-list-item-content>
-                <v-list-item class='overline font-weight-bold' style='font-size: small !important;' v-text="user.name"></v-list-item>
+                <v-list-item
+                  class="overline font-weight-bold"
+                  style="font-size: small !important;"
+                  v-text="user.name"
+                ></v-list-item>
               </v-list-item-content>
 
-              <v-list-item-action class='overline'>
+              <v-list-item-action class="overline">
                 {{ user.date }}
               </v-list-item-action>
             </v-list-item>
@@ -38,42 +47,101 @@
           </v-list>-->
         </v-card>
       </v-col>
-      <v-col cols="8" v-if='selectedUser'>
-        <v-list>
-          <v-list-item>
-            <v-list-item-title>
-              {{ selectedUser.name }}
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
+      <v-col cols="8" v-if="selectedUser">
+        <v-card elevation="4">
+          <v-card-title>
+            {{ selectedUser.name }}
+          </v-card-title>
+          <v-list
+            id="blep"
+            style="max-height: 30vh"
+            class="overflow-y-auto d-flex flex-column-reverse"
+          >
+            <!--           id:96
+            message:"a"
+            read:false
+            recipient:4
+            sender:5
+            sent:"2020-01-16T17:09:35.473+0000"-->
+            <v-list-item v-for="message in selectedUser.messages">
+              <v-spacer v-if="message.sender !== $auth.user().id"></v-spacer>
+              <v-list-item-content class='align-content-end'>{{ message.message }}</v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-form @submit.prevent="sendMessage" class="flex flex-wrap">
+              <v-text-field
+                clearable
+                hide-details
+                outlined
+                dense
+                v-model="message"
+                class="ma-2"
+              ></v-text-field>
+              <v-btn outlined class="ma-2" type="submit" color="primary"
+                >send
+              </v-btn>
+            </v-form>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { restApi } from '../../api/restApi';
+
 export default {
   name: 'Chat',
   data() {
     return {
       selectedUser: null,
+      message: null,
       users: [
         {
-          name: 'huu',
+          id: 4,
+          name: 'Archangel',
           avatar: '',
+          messages: [],
           date: '21.06.2019',
         },
-        { name: 'bleop', avatar: '', date: '21.06.2019' },
-        { name: 'annnn', avatar: '', date: '21.06.2019' },
-        { name: 'kiep', avatar: '', date: '21.06.2019' },
+        {
+          id: 5,
+          name: 'FochMaiden',
+          avatar: '',
+          messages: [],
+          date: '21.06.2019',
+        },
       ],
     };
   },
   methods: {
+    scrollToEnd() {
+      let container = this.$el.querySelector('#blep');
+      container.scrollTop = container.scrollHeight;
+    },
     selectUser(user) {
+      console.log('select user', user);
       this.selectedUser = user;
-
-    }
+      this.getMessages();
+    },
+    sendMessage() {
+      restApi.sendMessage(this.message, this.selectedUser.id).then(r => {
+        this.message = null;
+        this.getMessages();
+      });
+    },
+    getMessages() {
+      restApi.getMessages(this.selectedUser.id).then(response => {
+        this.selectedUser = {
+          ...this.selectedUser,
+          messages: response,
+        };
+        this.scrollToEnd();
+      });
+    },
   },
 };
 </script>
